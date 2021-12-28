65c7d0eefb007871574c833b1b930dbf5497bc4c
Samroman13 <96724884+Samroman13@users.noreply.github.com>
2021-12-28 13:18:13 +0300

    Add files vie upload
    Assembling files on 12/28/2021

---
+++ app.py
@@ -0,0 +1,19 @@
+
+
+import streamlit as st
+from PIL import Image 
+from segment import process
+
+st.title('Cifar_10 demo')
+
+image_file = st.file_uploader('Load an image', type=['png', 'jpg'])  # Adding a file loader
+
+if not image_file is None:                                           # Block execution if an image is loaded
+    col1, col2 = st.beta_columns(2)                                  # Creating 2 columns
+    image = Image.open(image_file)                                   # Image opening
+    pred_img, pred_text,  = process(image_file)                      # Image processing using a function implemented in another file
+    col1.text('Source image')
+    col1.image(image_file)                                           # Output in the first column of the original image
+    col2.text(pred_text)                                             # Output of the text prediction of the model
+    col2.image(pred_img)                                             # Output of the reduced source image to the second column
+    
---
+++ predict_img.py
@@ -0,0 +1,20 @@
+
+
+from tensorflow.keras.models import load_model
+MODEL_NAME =   'model_cf10.h5'
+import numpy as np
+from PIL import Image 
+model = load_model(MODEL_NAME)                                              
+INPUT_SHAPE = (32, 32, 3)
+classes = ['airplane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+def process(image_file):
+    
+    image = Image.open(image_file)  # Opening the processed file
+    resized_image = image.resize((INPUT_SHAPE[1], INPUT_SHAPE[0]))          # Resizing the image according to the network input
+    array = np.array(resized_image)[..., :3][np.newaxis, ..., np.newaxis]   # Adjusting the tensor shape for feeding to the network
+    predict = np.argmax(model.predict(array), axis=-1)
+    predict_text = classes[np.argmax(predict)]
+    
+
+    return resized_image, predict_text                          # Return of the original thumbnail image, text description of the prediction

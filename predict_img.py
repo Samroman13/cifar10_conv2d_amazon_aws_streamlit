456fcbb560077a9068f4e05384934663abfed77b
Samroman13 <96724884+Samroman13@users.noreply.github.com>
2021-12-29 17:03:12 +0300

    fixed a bug
    fixed dimension

--- predict_img.py
+++ predict_img.py
@@ -12,9 +12,10 @@ def process(image_file):
     
     image = Image.open(image_file)  # Opening the processed file
     resized_image = image.resize((INPUT_SHAPE[1], INPUT_SHAPE[0]))          # Resizing the image according to the network input
-    array = np.array(resized_image)[..., :3][np.newaxis, ..., np.newaxis]   # Adjusting the tensor shape for feeding to the network
-    predict = np.argmax(model.predict(array), axis=-1)
+    print(type(resized_image))
+    array = np.array(resized_image)[..., :3] [np.newaxis, ...]   # Adjusting the tensor shape for feeding to the network
+    print(array.shape)
+    predict = np.argmax(model.predict(array))
     predict_text = classes[np.argmax(predict)]
-    
-
+  
     return resized_image, predict_text                          # Return of the original thumbnail image, text description of the prediction
